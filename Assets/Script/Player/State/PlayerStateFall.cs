61798efe8ec226cc0f5ced42595ffd41885ede06
neeeeding <abc01096780866@gmail.com>
2025-02-02 00:00:25 +0900

    Add Fall State

---
+++ Assets/Script/Player/State/PlayerStateFall.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateFall : PlayerState
+{
+    public PlayerStateFall(string animationName, PlayerStateMachin stateMachin, Player player) : base(animationName, stateMachin, player)
+    {
+    }
+}
