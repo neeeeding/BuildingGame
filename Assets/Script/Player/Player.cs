a4e1e26fea2bbdfbd63508aa2f6fc07b68d803a3
neeeeding <abc01096780866@gmail.com>
2025-02-02 23:46:16 +0900

    Add GroundCheck

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private string currentState;
 
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private Vector3 groundCheckSize;
+
     private Animator animator;
     public Animator Animator => animator;
     private Rigidbody rigid;
@@ -22,7 +26,7 @@ public class Player : MonoBehaviour
         stateMachin.AddState(PlayerStateEnum.Idle,new PlayerStateIdle("Idle",stateMachin,this));
         stateMachin.AddState(PlayerStateEnum.Walk,new PlayerStateWalk("Walk", stateMachin, this));
         stateMachin.AddState(PlayerStateEnum.Jump,new PlayerStateJump("Jump", stateMachin, this));
-        //stateMachin.AddState(PlayerStateEnum.Fall,new PlayerStateFall("Fall", stateMachin, this));
+        stateMachin.AddState(PlayerStateEnum.Fall,new PlayerStateFall("Fall", stateMachin, this));
         //stateMachin.AddState(PlayerStateEnum.PickUp,new PlayerStatePickUp("PickUp", stateMachin, this));
         //stateMachin.AddState(PlayerStateEnum.Use,new PlayerStateUse("Use", stateMachin, this));
         stateMachin.Init(PlayerStateEnum.Idle);
@@ -38,5 +42,21 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         stateMachin.currentState.FixedUpdateState();
+    }
+
+    public bool CheckGround()
+    {
+        Collider[] collider = Physics.OverlapBox(groundCheck.position, groundCheckSize, Quaternion.identity, ground);
+        if(collider.Length > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(groundCheck.position, groundCheckSize);
     }
 }
