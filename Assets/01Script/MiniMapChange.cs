79ba04dd1340e4cf0ed921d2fad29d5cf47c2359
neeeeding <abc01096780866@gmail.com>
2025-02-08 23:34:45 +0900

    Add MiniMap

---
+++ Assets/01Script/MiniMapChange.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMapChange : MonoBehaviour
+{
+    [SerializeField] private BlueprintSO blueprintImage;
+
+    [SerializeField] private Image blueprint;
+    [SerializeField] private GameObject miniMap;
+
+    private bool isMap; //true = map, false = blueprint
+
+    private void Start()
+    {
+        isMap = false;
+
+        blueprint = blueprintImage.image;
+
+        miniMap.SetActive(isMap);
+        blueprint.gameObject.SetActive(!isMap);
+    }
+
+    public void ClcikBtn()
+    {
+        isMap = !isMap;
+
+        miniMap.SetActive(isMap);
+        blueprint.gameObject.SetActive(!isMap);
+    }
+}
